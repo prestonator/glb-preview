39e5dc910a80d5a0e49e7df76eae826df0e35e56
Preston Jenkins <preston@eltonjenkinslaw.com>
2026-07-22 12:43:19 -0500

    feat: add Scene component with configurable custom environment and 360-degree background texture

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,10 +8,10 @@ import { Model } from "./Homestead";
 function CustomEnvironment() {
   // Load custom sky image
   const texture = useTexture("/bgSky.png");
-  
+
   // Tell Three.js to wrap this image around a 360-degree sphere
   texture.mapping = THREE.EquirectangularReflectionMapping;
-  
+
   return (
     <>
       {/* Apply forest HDRI lighting to maintain model appearance */}
@@ -29,15 +29,11 @@ interface SceneProps {
 
 export function Scene({ useCustomEnv = true, currentStage = 1 }: SceneProps) {
   return (
-    <Canvas camera={{ position: [50, 50, 50], fov: 50 }}>
+    <Canvas camera={{ position: [40, 28, 58], fov: 50 }}>
       <Suspense fallback={null}>
         <Model currentStage={currentStage} />
-        
-        {useCustomEnv ? (
-          <CustomEnvironment />
-        ) : (
-          <Environment preset="forest" />
-        )}
+
+        {useCustomEnv ? <CustomEnvironment /> : <Environment preset="forest" />}
       </Suspense>
 
       {/* Free orbit controls */}
